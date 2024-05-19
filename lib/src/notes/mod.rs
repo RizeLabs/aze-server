c3ea188abbf3fa464fe23289b63551825e276cb5
Rachit Chahar <202151123@iiitvadodara.ac.in>
2024-05-19 07:42:45 +0530

    e2e test

--- lib/src/notes/mod.rs
+++ lib/src/notes/mod.rs
@@ -68,6 +68,32 @@ pub fn create_send_card_note<R: FeltRng, N: NodeRpcClient, S: Store>(
     Ok(Note::new(vault, metadata, recipient))
 }
 
+pub fn create_play_bet_note<R: FeltRng, N: NodeRpcClient, S: Store>(
+    client: &mut Client<N, R, S>,
+    sender_account_id: AccountId,
+    target_account_id: AccountId,
+    assets: Vec<Asset>,
+    note_type: NoteType,
+    mut rng: RpoRandomCoin,
+    player_bet: u8
+) -> Result<Note, NoteError> {
+    let note_script = include_str!("../../contracts/notes/game/bet.masm");
+    let script_ast = ProgramAst::parse(note_script).unwrap();
+    let note_script = client.compile_note_script(script_ast, vec![]).unwrap();
+
+    let inputs = vec![Felt::from(player_bet)];
+    let note_inputs = NoteInputs::new(inputs).unwrap();
+    let tag = NoteTag::from_account_id(target_account_id, NoteExecutionMode::Local)?;
+    let serial_num = rng.draw_word();
+    let aux = ZERO;
+
+    let metadata = NoteMetadata::new(sender_account_id, NoteType::Public, tag, aux)?;
+    let vault = NoteAssets::new(assets)?;
+    let recipient = NoteRecipient::new(serial_num, note_script, note_inputs);
+
+    Ok(Note::new(vault, metadata, recipient))
+}
+
 pub fn create_play_raise_note<R: FeltRng, N: NodeRpcClient, S: Store>(
     client: &mut Client<N, R, S>,
     sender_account_id: AccountId,
